64dd8ae2edc0657858a317ca108231d56f6bc3a6
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-04-20 08:37:36 -0400

    Initial Commit

---
+++ BookTracker/BookTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackerApp.swift
+//  BookTracker
+//
+//  Created by Emanuel Gross von Trockau on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
